a31909529c57e03d1b8779a3294a3a0ec23ae67c
YoussefWilliam <youssef.william997@gmail.com>
2022-05-15 12:43:10 +0200

    add insert and delete functions

--- linkedlists.js
+++ linkedlists.js
@@ -26,11 +26,53 @@ class LinkedList {
     this.length = this.length + 1;
     return this;
   }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    let currentNode = this.head;
+    let i = 0;
+    while (i < index && currentNode !== null) {
+      currentNode = currentNode.next;
+      i++;
+    }
+    const newNode = new Node(value);
+    const theVeryNextNode = currentNode.next;
+    currentNode.next = newNode;
+    newNode.next = theVeryNextNode;
+    this.length++;
+    return this;
+  }
+  delete(index) {
+    let currentNode = this.head;
+    let i = 0;
+    while (i < index - 1 && currentNode !== null) {
+      currentNode = currentNode.next;
+      i++;
+    }
+    currentNode.next = currentNode.next.next;
+    this.length--;
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
-myLinkedList.prepend(7);
-
-console.log("🚀 ~ file: linkedlists.js ~ line 15 ~ myLinkedList", myLinkedList);
+myLinkedList.insert(1, 99);
+myLinkedList.insert(3, 77);
+console.log(
+  "🚀 ~ file: linkedlists.js ~ line 15 ~ myLinkedList",
+  myLinkedList.printList()
+);
+myLinkedList.delete(1);
+console.log(
+  "🚀 ~ file: linkedlists.js ~ line 15 ~ myLinkedList",
+  myLinkedList.printList()
+);
