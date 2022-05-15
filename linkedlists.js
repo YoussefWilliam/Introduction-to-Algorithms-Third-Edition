d67d0f3791b0911e3016b67ea8443ec7c87fb8dc
YoussefWilliam <youssef.william997@gmail.com>
2022-05-15 12:02:45 +0200

    add class to DRY

--- linkedlists.js
+++ linkedlists.js
@@ -1,31 +1,30 @@
 // 10 -> 5 -> 16
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class LinkedList {
   constructor(value) {
-    this.head = {
-      value: value,
-      next: null,
-    };
+    this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
   }
   append(value) {
-    const newItem = {
-      value: value,
-      next: null,
-    };
-    this.tail.next = newItem;
-    this.tail = newItem;
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
     this.length = this.length + 1;
+    return this;
   }
   prepend(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length = this.length + 1;
+    return this;
   }
 }
 
