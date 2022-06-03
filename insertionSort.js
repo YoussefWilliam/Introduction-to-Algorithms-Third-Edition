89af03a659fbf89ce86e218c825f4a981c4d48c5
YoussefWilliam <youssef.william997@gmail.com>
2022-06-03 12:27:13 +0200

    apply insertion sort

--- insertionSort.js
+++ insertionSort.js
@@ -1,25 +1,24 @@
 /**
- * In insertion sort, you compare the previous number with the current number.
+ * In this sorting algorithm, we look at the next item and compare it with the prev item.
  * If the current number is smaller than the previous number => you swap.
  * Else => you break
  */
 
 function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
+  const length = arr.length;
+  for (let i = 1; i < length; i++) {
     for (let j = i; j > 0; j--) {
       let currentNumber = arr[j];
       let previousNumber = arr[j - 1];
       if (currentNumber < previousNumber) {
-        let temp = arr[j];
+        let tempNumber = arr[j];
         arr[j] = arr[j - 1];
-        arr[j - 1] = temp;
-      } else {
-        break;
+        arr[j - 1] = tempNumber;
       }
     }
   }
   return arr;
 }
 
-const arr = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+const arr = [0, 99, 44, 6, 2, 1, 5, 63, 87, 283, 4];
 console.log(insertionSort(arr));
