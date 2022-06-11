ed469c9146ae860041ef3f24bea4bc697bfb443c
YoussefWilliam <youssef.william997@gmail.com>
2022-06-11 17:03:32 +0200

    adding depth first search

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -3,9 +3,11 @@
  *      4               20
  *   1       6       15      170
  *
- * Breadth First search => [9,4,20,1,6,15,170]
- * Depth First search   => [9,4,1,6,20,15,170]
- *
+ * Breadth First search => [9, 4, 20, 1, 6, 15, 170]
+ * Depth First search has 3 types:
+ *    1. InOrder   ->  [1, 4, 6, 9, 15, 20, 170] => Ordered binary list
+ *    2. PreOrder  ->  [9, 4, 1, 6, 20, 15, 170] => Normal depth first search
+ *    3. PostOrder ->  [1, 6, 4, 15, 170, 20, 9] => From children to parent order.
  */
 
 class Node {
@@ -122,7 +124,7 @@ class BinarySearchTree {
     }
   }
 
-  // return a list of traversing the tree in breadth first search.
+  // return a list of traversing iteratively the tree in breadth first search.
   breadthFirstSearch() {
     let currentNode = this.root;
     let result = [];
@@ -141,6 +143,7 @@ class BinarySearchTree {
     return result;
   }
 
+  // return a list of traversing recursively the tree in breadth first search.
   recursiveBreadthFirstSearch(memoryQueue, result) {
     if (memoryQueue.length === 0) {
       return result;
@@ -156,6 +159,53 @@ class BinarySearchTree {
 
     return this.recursiveBreadthFirstSearch(memoryQueue, result);
   }
+
+  // return a list of traversing recursively the tree in depth first search using the in-order technique
+  depthFirstSearchInOrder() {
+    return traverseInOrder(this.root, []);
+  }
+
+  // return a list of traversing recursively the tree in depth first search using the pre-order technique
+  depthFirstSearchPreOrder() {
+    return traversePreOrder(this.root, []);
+  }
+
+  // return a list of traversing recursively the tree in depth first search using the post-order technique
+  depthFirstSearchPostOrder() {
+    return traversePostOrder(this.root, []);
+  }
+}
+
+function traverseInOrder(currentNode, result) {
+  if (currentNode.left) {
+    traverseInOrder(currentNode.left, result);
+  }
+  result.push(currentNode.value);
+  if (currentNode.right) {
+    traverseInOrder(currentNode.right, result);
+  }
+  return result;
+}
+function traversePreOrder(currentNode, result) {
+  result.push(currentNode.value);
+  if (currentNode.left) {
+    traversePreOrder(currentNode.left, result);
+  }
+  if (currentNode.right) {
+    traversePreOrder(currentNode.right, result);
+  }
+  return result;
+}
+function traversePostOrder(currentNode, result) {
+  if (currentNode.left) {
+    traversePostOrder(currentNode.left, result);
+  }
+
+  if (currentNode.right) {
+    traversePostOrder(currentNode.right, result);
+  }
+  result.push(currentNode.value);
+  return result;
 }
 
 const myTree = new BinarySearchTree();
@@ -168,12 +218,16 @@ myTree.insert(15);
 myTree.insert(170);
 
 // console.log(JSON.stringify(traverse(myTree.root)));
+console.log(myTree.breadthFirstSearch());
 console.log(myTree.recursiveBreadthFirstSearch([myTree.root], []));
+console.log(myTree.depthFirstSearchInOrder());
+console.log(myTree.depthFirstSearchPostOrder());
+console.log(myTree.depthFirstSearchPreOrder());
 
-// console.log(myTree.lookup(9));
-// console.log(myTree.lookup(4));
+console.log(myTree.lookup(9));
+console.log(myTree.lookup(4));
 
-// myTree.remove(20);
-// console.log(myTree.lookup(20));
+myTree.remove(20);
+console.log(myTree.lookup(20));
 
-// console.log(JSON.stringify(traverse(myTree.root)));
+console.log(JSON.stringify(traverse(myTree.root)));
